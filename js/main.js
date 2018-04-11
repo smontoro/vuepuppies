885b738fe31be226f6339c06d4da808b47c47453
smontoro <sarahjmontoro@gmail.com>
2018-04-10 22:18:23 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,44 +1,11 @@
-new Vue({
-	el: '#app',
+var todo = new Vue({
+	el: '#todo',
 	data: {
-		puppies: [
-			{
-				name: 'Lilly',
-				gender: 'female',
-				age: 0.7,
-				breed: 'Italian Greyhound',
-				weightKG: 7,
-				image: 'https://i.pinimg.com/originals/4c/90/f3/4c90f3aa526a041df9540b34cb3108fb.jpg'
-			},
-			{
-				name: 'Barney',
-				gender: 'male',
-				age: 0.9,
-				breed: 'Beagle',
-				weightKG: 7,
-				image: 'https://i.ytimg.com/vi/bx7BjjqHf2U/maxresdefault.jpg',
-				special: 'Mute'
-			},
-			{
-				name: 'NuoMi',
-				gender: 'female',
-				age: 0.4,
-				breed: 'Pomeranian',
-				weightKG: 4,
-				image: 'https://global-free-classified-ads-s02.r.worldssl.net/user_images/6236573.jpg',
-				special: '3-legged'
-			},
-			{
-				name: 'Ted',
-				gender: 'female',
-				age: 0.5,
-				breed: 'Basenji',
-				weightKG: 4,
-				image: 'https://thumbs.dreamstime.com/b/basenji-valp-66605093.jpg',
-			}
+		newTask:'',
+		todoList: []
+	},
+	method: {
 
-			]
-	}, 
-	
+	},
 })
 
